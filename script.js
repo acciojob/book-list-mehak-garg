c336bc68d96458218fd52ddac1c3c3c4b680e761
mehak-garg <mehakgarg741@gmail.com>
2026-04-04 16:13:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+const a=document.querySelector("#book-list");
+let b=document.querySelector("#title");
+let c=document.querySelector("#author");
+let d=document.querySelector("#isbn");
+const e=document.querySelector("#submit");
+
+e.addEventListener("click",add_func);
+
+
+function add_func(){
+    a.innerHTML+=`<tr><td>${b.value}</td><td>${c.value}</td><td>${d.value}</td><td><button class="delete">*</button></td></tr>`;
+}
+
+document.getElementById('book-list').addEventListener('click', function(e) { if (e.target.classList.contains('delete')) { e.target.parentElement.parentElement.remove(); } });
